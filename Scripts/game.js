88ffce209d5447147873f77e9051128013c9ed25
MagicScorp <imamovic.kerim1@gmail.com>
2020-01-09 22:32:05 +0100

    Changed game.js

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,30 +1,11 @@
-var canvas = document.getElementById("platno")
-var ctx = canvas.getContext('2d')
 
-ctx.strokeStyle = 'black';
-ctx.lineWidth = 1
-ctx.font = "30px Arial"
-for (i = 80;i<800;i+=160){
-    for (j = 80;j<800;j+=160){
-    printOutRandNum(i,j);
-    }
-}
-
-/* for (i = 0; i < 800; i+=160) {
-    ctx.moveTo(i,0);
-    ctx.lineTo(i,800);
-    ctx.stroke();
-}
-
-for (j = 0; j < 800; j+=160) {
-    ctx.moveTo(0,j);
-    ctx.lineTo(800,j);
-    ctx.stroke();
-} */
 function generateRandomNumb(min,max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-function printOutRandNum(x,y) {
-    ctx.fillText(generateRandomNumb(1,11), x,y);
+for (var i = 1;i<26;i++){
+    document.getElementById(String(i)).innerHTML = generateRandomNumb(1,11);
 }
+
+var P1Sum = 0
+var P2Sum = 0
+
