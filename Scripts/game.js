56c889ba9a0f1853acb4953daaf9e9faeda15ca5
MagicScorp <imamovic.kerim1@gmail.com>
2020-01-09 18:30:23 +0100

    Centered canvas,added new font,font size

--- Scripts/game.js
+++ Scripts/game.js
@@ -3,14 +3,14 @@ var ctx = canvas.getContext('2d')
 
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 1
-
+ctx.font = "30px Arial"
 for (i = 80;i<800;i+=160){
     for (j = 80;j<800;j+=160){
     printOutRandNum(i,j);
     }
 }
 
-for (i = 0; i < 800; i+=160) {
+/* for (i = 0; i < 800; i+=160) {
     ctx.moveTo(i,0);
     ctx.lineTo(i,800);
     ctx.stroke();
@@ -20,7 +20,7 @@ for (j = 0; j < 800; j+=160) {
     ctx.moveTo(0,j);
     ctx.lineTo(800,j);
     ctx.stroke();
-}
+} */
 function generateRandomNumb(min,max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
