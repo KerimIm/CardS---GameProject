979a9e7796c772f8da449214873b9704d940e20e
MagicScorp <imamovic.kerim1@gmail.com>
2020-01-09 18:10:46 +0100

    Added numbers to cells

--- Scripts/game.js
+++ Scripts/game.js
@@ -4,7 +4,11 @@ var ctx = canvas.getContext('2d')
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 1
 
-
+for (i = 80;i<800;i+=160){
+    for (j = 80;j<800;j+=160){
+    printOutRandNum(i,j);
+    }
+}
 
 for (i = 0; i < 800; i+=160) {
     ctx.moveTo(i,0);
@@ -16,12 +20,11 @@ for (j = 0; j < 800; j+=160) {
     ctx.moveTo(0,j);
     ctx.lineTo(800,j);
     ctx.stroke();
-}
 
 function generateRandomNumb(min,max) {
-    Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function printOutRandNum(x,y,z) {
-
+function printOutRandNum(x,y) {
+    ctx.fillText(generateRandomNumb(1,11), x,y);
 }
