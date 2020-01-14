e272bd409b6d1a926438d272e8043926b31ee77b
MagicScorp <imamovic.kerim1@gmail.com>
2020-01-14 16:04:44 +0100

    Added feature where if win condition is met players are prompted with confirm screen and if they confirm player who was playing seccond plays first now

--- Scripts/game.js
+++ Scripts/game.js
@@ -4,20 +4,10 @@ var P2Sum = 0;
 var arr = [];
 var krajIgre = false;
 
-for (var i = 0; i < 17; i++){ //generete 17 numbers between 1-10
-    var x = generateRandomNumb(1,11);
-    arr.push(x);
+function generateRandomNumb(min,max) { //generate random val from 1 to 10
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-for (var j = 0; j < 8; j++){
-    if (j < 4){
-        arr.push(11);
-    }
-    else {
-        arr.push(12);
-    }
-}
-console.log(arr);
 //algoritham for shuffeling elements in array found on link below
 function shuffleArray(arr) { // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     for (var i = arr.length - 1; i > 0; i--) {
@@ -27,11 +17,33 @@ function shuffleArray(arr) { // https://stackoverflow.com/questions/2450954/how-
         arr[j] = temp;
     }
 }
+
+function generateArray(arr){
+    for (var i = 0; i < 17; i++){ //generete 17 numbers between 1-10
+        var x = generateRandomNumb(1,11);
+        arr.push(x);
+    }
+    for (var j = 0; j < 8; j++){
+        if (j < 4){
+            arr.push(11);
+        }
+        else {
+            arr.push(12);
+        }
+    }
+    return arr;
+}
+generateArray(arr);
 shuffleArray(arr);
-console.log(arr);
+for (var i = 0;i<25;i++){ // adding random values to buttons
+    document.getElementById(String(i)).innerHTML = arr[i];
+}
+
+
+
 function switchElements(arr,id){ //switching between elements if value passed is 11(smile) or 12(angry) it switches that value with some other from the array
     console.log(arr[id]);
-    var j = generateRandomNumb(0,26);
+    var j = generateRandomNumb(0,25);
     while (arr[j] === 11 || arr[j] === 12) {
         j = generateRandomNumb(0,26)
     }
@@ -42,23 +54,6 @@ function switchElements(arr,id){ //switching between elements if value passed is
     document.getElementById(String(id)).innerHTML = arr[id];
     document.getElementById(String(j)).innerHTML = arr[j];
 }// it appears in some cases that function isn't working 
-function generateRandomNumb(min,max) { //generate random val from 1 to 10
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-
-for (var i = 0;i<25;i++){ // adding random values to buttons
-    document.getElementById(String(i)).innerHTML = arr[i];
-}
-
-function winner(x) { //function which decides who won
-    if (x >= 50) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
 
 function whoIsPlaying(num){ //function for marking the current player
     if (plays === 1){
@@ -81,57 +76,105 @@ if (P1Sum === 0 || P2Sum === 0) {
     document.getElementById("sum1").innerHTML = P1Sum;
     document.getElementById("sum2").innerHTML = P2Sum;
 }
+
+function reRun(){
+    P1Sum = 0;
+    P2Sum = 0;
+    document.getElementById("sum1").innerHTML = P1Sum;
+    document.getElementById("sum2").innerHTML = P2Sum;
+    generateArray(arr);
+    shuffleArray(arr);
+    for (var i = 0;i<25;i++){ // adding random values to buttons
+        document.getElementById(String(i)).innerHTML = arr[i];
+    }
+    klikAndSum(id);
+}
+
+function winner(x,y) { //function which decides who won
+    if (x >= 50) {
+        alert("Winner is player 1 and he has: " + P1Sum + " points" );
+        var conf = confirm("Do you want a rematch?!");
+        if (conf){
+        plays *= 1;
+        whoIsPlaying(plays);
+        reRun();
+        }
+        else {
+            window.location.reload();
+        }
+    }
+    else if (y >= 50) {
+        alert("Winner is player 2 and he has: " + P2Sum + " points" );
+        var conf = confirm("Do you want a rematch?!");
+        if (conf){
+            plays *= 1;
+            whoIsPlaying(plays);
+            reRun();
+        }
+        else {
+        window.location.reload();
+        }
+    }
+}
+
 whoIsPlaying(plays);
 function klikAndSum(id) { // adds value to sum on click and displays the value
-    
-
     var num = (arr[id]);
     if (plays === 1) { 
         if (num === 11){
+            P1Sum *= 2;
             plays *= -1;
             switchElements(arr,id);
             whoIsPlaying(plays);
-            return document.getElementById("sum1").innerHTML = P1Sum * P1Sum;
+            winner(P1Sum,P2Sum);
+            return document.getElementById("sum1").innerHTML = P1Sum;
         }
         else if (num === 12){
             plays *= -1;
             P1Sum = 0;
             switchElements(arr,id);
             whoIsPlaying(plays);
+            winner(P1Sum,P2Sum);
             return document.getElementById("sum1").innerHTML = P1Sum;
         }
         else {
             P1Sum += num;
             plays *= -1;
             whoIsPlaying(plays);
+            winner(P1Sum,P2Sum);
             return document.getElementById("sum1").innerHTML = P1Sum;   
         }
     }
     if (plays === -1) {
         if (num === 11){
+            P2Sum *= 2;
             plays *= -1;
             switchElements(arr,id);
             whoIsPlaying(plays);
-            return document.getElementById("sum2").innerHTML = P2Sum * P2Sum;
+            winner(P1Sum,P2Sum);
+            return document.getElementById("sum2").innerHTML = P2Sum;
         }
         else if (num === 12){
             P2Sum = 0;
             plays *= -1;
             switchElements(arr,id);
             whoIsPlaying(plays);
+            winner(P1Sum,P2Sum);
             return document.getElementById("sum2").innerHTML = P2Sum;
         }
         else {
             P2Sum += num;
             plays *= -1;
             whoIsPlaying(plays);
+            winner(P1Sum,P2Sum);
             return document.getElementById("sum2").innerHTML = P2Sum;
-        
         }
     }
 }
 
-
+/* if(winner(P1Sum,P2Sum)){
+    
+} */
 
 
 
